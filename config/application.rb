48a407f54343c9c0805708445abd92050f027059
Lubmes <l.m.schukking@gmail.com>
2017-06-02 12:20:36 +0200

    I18n opzet.

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Poetry
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Vertaling met I18n.
+    config.i18n.default_locale = :nl
   end
 end
